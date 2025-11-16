261e000d3b9b5601bcebdefd582e8d5adaa9ecb9
Preet <kumargpreet775@gmail.com>
2025-11-16 08:29:28 +0530

    dummy commit

---
+++ src/main/java/health/com/HealthCareApplication/HealthCareApplication.java
@@ -0,0 +1,14 @@
+package health.com.HealthCareApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HealthCareApplication.class, args);
+	}
+
+}
